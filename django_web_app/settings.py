d1049e83554b5b96be8d125eeee4ff12cc1188f1
Humoyun004 <humoyunakbaraliev1@gmail.com>
2023-07-19 21:10:55 +0500

    Updated:Добавил несколько изменений

--- django_web_app/settings.py
+++ django_web_app/settings.py
@@ -136,3 +136,12 @@ LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
 LOGOUT_REDIRECT_URL = 'login'
+
+# Email
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'humoyunakbaraliyev2159555@gmail.com'
+EMAIL_HOST_PASSWORD = 'riklryancckcsmjy'
